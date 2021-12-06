a7190fe12e581f3a2fa29baea198fc293237cfac
unknown <flavio900@live.com>
2021-12-06 12:08:32 -0300

    Consumo de API - Login e Cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
-import { Component, NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
 
   {path: '', redirectTo: 'entrar', pathMatch: 'full'},
 
   {path:'entrar', component: EntrarComponent},
-  {path: 'cadastrar', component: CadastrarComponent}
-]
-;
+  {path: 'cadastrar', component: CadastrarComponent},
 
+  {path: 'inicio', component: InicioComponent}
 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
